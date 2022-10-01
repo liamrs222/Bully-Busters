b8917715f507ede1842b16c5fce200ea45ef6d05
Liam Sy <79372899+liamrs222@users.noreply.github.com>
2022-10-01 03:35:32 -0400

    Add files via upload

---
+++ language_censor.js
@@ -0,0 +1,16 @@
+
+replaceText(document.body)
+
+function replaceText(element) {
+    if(element.hasChildNodes()) {
+        element.childNodes.forEach(replaceText)
+    }
+    else if (element.nodeType === Text.TEXT_NODE) {
+        if(element.textContent.match(/water/gi)) {
+            const newElement = document.createElement('span')
+            newElement.innerHTML = element.textContent.replace(/(water)/gi,
+            '<span style="background-color: black; color: black;>$1</span>')
+            element.replaceWith(newElement)
+        }
+    }
+}
