67eafe4f81d753c36b59489a54d8ee3c17b8ca51
Derek Mo <derek.mo7203@gmail.com>
2022-10-01 18:50:13 -0400

    comments

--- background.js
+++ background.js
@@ -1,6 +1,10 @@
+/*
+scrapped attempt at making another window show up
+
 chrome.action.onClicked.addListener(function(tab) {
     chrome.windows.create({
-      url: chrome.runtime.getURL("warning.html"),
+      url: chrome.runtime.getURL("Bully-Busters/warning.html"),
       type: "popup"
     });
 });
+*/
--- content.js
+++ content.js
@@ -1,6 +1,11 @@
+/*
+former image censor
+
 let blackScreen = "https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/A_black_image.jpg/1200px-A_black_image.jpg";
 
 const imgs = document.getElementsByTagName("img");
 for (let i = 0; i < imgs.length; i++) {
     imgs[i].src = blackScreen;
 }
+*/
+alert ("listen here you booger, stop being a d*ckhead and BE NICE")
