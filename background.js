c0c29b06f6745f32620ea26681d3e46b83b85fc9
Jeremy Garcia <73523692+jareuhmee@users.noreply.github.com>
2022-10-02 04:52:26 -0400

    Toggle sends automatic changes

--- background.js
+++ background.js
@@ -1,14 +1,33 @@
-var isToggled;
+var isToggled = false;
 
+// when toggled, update script
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.message === 'yo the button turned on') {
-        chrome.storage.local.get("enabled", value => {
-            isToggled = value;
-            console.log (value);
+        chrome.storage.local.get("enabled", function (result) {
+            isToggled = result.enabled;
             console.log ("message received baby");
+            console.log (isToggled);
+
+            if (isToggled) {
+                chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
+                    console.log(tabs[0].url);
+                    my_tabid=tabs[0].id;
+
+                    chrome.scripting.executeScript({
+                        files: ['language_censor.js'],
+                        target: {tabId: my_tabid}
+                    });
+                  });
+            }
+            else {
+                chrome.tabs.query({active: true, currentWindow: true}, function (arrayOfTabs) {
+                    chrome.tabs.reload(arrayOfTabs[0].id);
+                });
+            }
         })
     }
 });
+
 
 //when a tab is activated, update script
 chrome.tabs.onActivated.addListener(tab => {
@@ -19,7 +38,6 @@ chrome.tabs.onActivated.addListener(tab => {
         },() => { 
             console.log('script injected') 
         });
-
     }
 });
 
--- popup.js
+++ popup.js
@@ -1,52 +1,43 @@
-const text = document.querySelectorAll('h1, h2, h3, h4, h5, p, li, td, caption, span, a');
-function replaceText(text) {
-    for (let i = 0; i < text.length; i++) {
-        if (text[i].innerHTML.includes('Fuck')) {
-            text[i].innerHTML = text[i].innerHTML.replace('Fuck', 'derek');
-        }
-        else if (text[i].innerHTML.includes('fuck')) {
-            text[i].innerHTML = text[i].innerHTML.replace('fuck', 'derek');
-        }
-        else if (text[i].innerHTML.includes('FUCK')) {
-            text[i].innerHTML = text[i].innerHTML.replace('FUCK', 'derek');
-        }
-    }
-};
-
-var isToggled;
+const toggle = document.querySelector('.container')
+var isToggled = false;
 
 document.addEventListener('DOMContentLoaded', function () {
     var checkbox = document.getElementById('toggle')
 
     checkbox.addEventListener('change', function () {
+    const refresh = toggle.parentNode.querySelector('.refresh')
     if (checkbox.checked) {
         isToggled = true;
-        replaceText(text);
+        refresh.textContent = isToggled ? 'Censoring is currently active' : 'Refresh page to unredact!'
         console.log('toggled');
-    } 
+    }
     else {
         isToggled = false;
+        refresh.textContent = isToggled ? 'Censoring is currently active' : 'Refresh page to unredact!'
         console.log('not toggled');
     }
     });
 });
 
+var change = false;
 document.addEventListener('DOMContentLoaded', function () {
     var checkbox = document.querySelector('input[type="checkbox"]');
     chrome.storage.local.get('enabled', function (result) {
         if (result.enabled != null) {
             checkbox.checked = result.enabled;
-            if(checkbox.checked)
-            {
-                replaceText(text);
+            if(checkbox.checked){
+                change = true;
             }
         }
     });
     checkbox.addEventListener('click', function () {
         console.log(checkbox.checked);
-        chrome.storage.local.set({ 'enabled': !isToggled }, function () {
+        chrome.storage.local.set({ 'enabled': isToggled }, function () {
             console.log("button is on");
         });
         chrome.runtime.sendMessage({message: 'yo the button turned on'});
+        chrome.storage.local.set({ 'enabled': checkbox.checked }, function () {
+            console.log("confirmed");
+        });
     });
 });
