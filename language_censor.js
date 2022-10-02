ea3c6e091cc7238da4c392d25348f02b5b902fa0
Jeremy Garcia <73523692+jareuhmee@users.noreply.github.com>
2022-10-02 01:07:19 -0400

    Added more blacklisted words

--- language_censor.js
+++ language_censor.js
@@ -1,5 +1,5 @@
 const text = document.querySelectorAll('h1, h2, h3, h4, h5, p, li, td, caption, span, a');
-const blacklistedWords = ['fuck', 'shit', 'bitch', 'dick', 'whore'];
+const blacklistedWords = ['fuck', 'shit', 'bitch', 'dick', 'whore', 'asshole', 'cocksucker'];
 
 function replaceWords(blacklistedWords) {
     for (let i = 0; i < text.length; i++) {
